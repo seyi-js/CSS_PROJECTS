160c98c149c8b5ad115e7e6190ec27f562958646
ADEBAYO SAMUEL OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2020-08-06 23:03:57 +0100

    Added responsiveness

--- public3/index.js
+++ public3/index.js
@@ -1,5 +1,7 @@
 const signIn = document.getElementById('signIn')
+const signInButton = document.getElementById('sign-In')
 const signUp = document.getElementById( 'signUp' )
+const signUpButton = document.getElementById( 'sign-Up' )
 const wrapper = document.getElementById( 'wrapper' )
 
 
@@ -14,5 +16,22 @@ signUp.addEventListener( 'click', () => {
 
     wrapper.classList.add( 'right-panel-active' );
 } );
+
+
+
+signInButton.addEventListener( 'click', () => {
+   
+    
+    wrapper.classList.add( 'right-panel-active' );
+} );
+
+
+signUpButton.addEventListener( 'click', () => {
+    
+    
+
+    wrapper.classList.remove( 'right-panel-active' );
+} );
+
 
 
