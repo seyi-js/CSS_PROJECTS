da8e79597c8e0b3da0be4f8a83662941569107ec
ADEBAYO SAMUEL OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2020-08-20 18:51:11 +0100

    Added some fonts to the previous project and also did some animation

--- public7/index.js
+++ public7/index.js
@@ -1,15 +1,26 @@
-window.onload = function(){
+// window.onload = function(){
+    
+// }
+
+window.addEventListener( 'load', () => {
     const bars = document.getElementById( 'BARS' );
     const mobileView = document.getElementById('mobile-view')
-bars.addEventListener( 'click', () => {
-    if ( mobileView.classList.contains( 'mobile-show' ) ) {
+    const afterBanner1 = document.getElementById('after-banner-section-1')
+    const afterBanner = document.getElementById('after-banner-section')
+    bars.addEventListener( 'click', () => {
+        if ( mobileView.classList.contains( 'mobile-show' ) ) {
         
-    mobileView.classList.remove( 'mobile-show' );
-    } else{
+            mobileView.classList.remove( 'mobile-show' );
+        } else {
         
-    mobileView.classList.add( 'mobile-show' );
-    }
+            mobileView.classList.add( 'mobile-show' );
+        }
+    } );
+
+    afterBanner.addEventListener( 'focus', () => {
+
+        afterBanner1.classList.add('slide-in')
+    })
 })
-}
 // console.log(document.onload)
 
